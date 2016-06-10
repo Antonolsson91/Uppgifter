a57953d0cd76ec92cd14a9746a38b3d1b8e81d83
Antonolsson91 <antonolsson91@yahoo.com>
2016-06-10 16:43:41 +0200

    Lagt till Logger och Employee class

---
+++ Uppgift9/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift9
+{
+    class Employee
+    {
+        private string firstName;
+        private int wage;
+        private string ssn;
+        private string lastName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Ssn
+        {
+            get { return ssn; }
+            set { ssn = value; }
+        }
+
+        public int Wage
+        {
+            get { return wage; }
+            set { wage = value; }
+        }
+        public Employee(string firstName, string lastName, string ssn, int wage)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Ssn = ssn;
+            Wage = wage;
+        }
+    }
+}
---
+++ Uppgift9/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komplettering
+{
+    class Logger
+    {
+        public void Log(string message)
+        {
+            LogPosts.Add(message);
+        }
+        public List<string> LogPosts { get; private set; }
+        public Logger()
+        {
+            LogPosts = new List<string>();
+        }
+    }
+}
