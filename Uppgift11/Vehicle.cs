8cb562a387b8e734e064d5758cd7d30a3d48b642
Antonolsson91 <antonolsson91@yahoo.com>
2016-11-01 01:44:55 +0100

    Uppgift 11 done and done

---
+++ Uppgift11/Vehicle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift11
+{
+     abstract class Vehicle
+    {
+        public string VehicleType { get; set;}
+        public Vehicle(string VehicleType)
+        {
+            this.VehicleType = VehicleType;
+        }
+        public virtual string DescribeVehicle()
+        {          
+            return ("The vehicles type is: " + VehicleType);
+        }
+    }
+    class Car : Vehicle
+
+    {
+        public bool engineIsRunning = false;
+        public Car(string type) : base("Car")
+        {
+            this.VehicleType = VehicleType;
+           
+        }
+        public void StartEngine()
+        {
+            engineIsRunning = true;
+                
+        }
+        public void StopEngine()
+        {
+            engineIsRunning = false;           
+        }
+        public override string DescribeVehicle()
+        {
+            return "The vehicles type is: " + this.VehicleType + "\nIs the engine running?" + engineIsRunning;
+        }
+    }
+    class Boat : Vehicle
+    {
+        public Boat(string VehicleType) : base("Boat")
+        {
+            this.VehicleType = VehicleType;
+        }
+        public override string DescribeVehicle()
+        {
+            
+            return "The vehicles type is: " + VehicleType;
+        }
+
+    }
+}
